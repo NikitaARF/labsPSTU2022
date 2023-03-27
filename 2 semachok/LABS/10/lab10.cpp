60263f8db82b305b60666a03f96a5db68d0a9697
NikitaARF <115221224+NikitaARF@users.noreply.github.com>
2023-03-27 12:23:35 +0500

    Add files via upload

---
+++ 2 semachok/LABS/10/lab10.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <string>
+#include <windows.h>
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	int size, k;
+	cout << "Enter size of array: ";
+	cin >> size;
+	string* arr = new string[size];
+	for (int i = 0; i < size; i++) {
+		cin >> arr[i];
+	}
+	for (int i = 0; i < size; i++) {
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+	cout << "enter K words need to delete from string: ";
+	cin >> k;
+	string* arr2 = new string[size - k];
+
+	for (int i = 0; i < size - k; i++) {
+		if (i <= k) {
+			arr2[i] = arr[i + k];
+		}
+		cout << arr2[i] << " ";
+	}
+	delete[] arr;
+	delete[] arr2;
+}
